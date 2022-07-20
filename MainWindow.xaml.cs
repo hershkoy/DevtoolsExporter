5205348d128c71590f9ce5b953267551039ab98e
Hezi H <hershkoy@gmail.com>
2022-07-20 12:38:53 +0300

    add the option for remote debugging

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace WPFSample
     {
         public MainWindow()
         {
+            Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--remote-debugging-port=9222");
             InitializeComponent();
             //webView.NavigationStarting += EnsureHttps;
             InitializeAsync();
